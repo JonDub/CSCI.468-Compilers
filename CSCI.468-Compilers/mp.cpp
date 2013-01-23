7e965558cd853830082cabef633176dcec85d399
Jon Wickens <jkwickens@gmail.com>
2013-01-22 20:57:53 -0700

    Initial commit

---
+++ CSCI.468-Compilers/mp.cpp
@@ -0,0 +1,14 @@
+// CSCI.468-Compilers.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main(int argc, char* argv)
+{
+	printf("Done. Press Enter to exit.");
+	std::cin.get();
+	return 0;
+}
+
