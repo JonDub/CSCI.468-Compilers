cc6aba325c59d9a50f1f4fa21fa103193f7072bf
smorrisonsmith <sarah.morrisonsmith@gmail.com>
2013-02-07 23:56:43 -0700

    Adjusted Match to take tokens as input, not strings

--- Project1/parser.cpp
+++ Project1/parser.cpp
@@ -4,9 +4,66 @@
 #include "mp.h"
 using namespace std;
 
+// note, use tokens as defined in http://www.cs.montana.edu/ross/classes/spring2008/cs450/pages/resources/tokens.html
+
 //deliberately left empty for now
 parser::parser(){}
 
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::SystemGoal()
+{
+	switch(lookahead)
+	{
+	case: MP_PROGRAM //SystemGoal --> Program eof, rule #1     
+	{
+		Program();
+		Match("MP_EOF");
+		break();
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::Program()
+{
+	switch(lookahead)
+	{
+	case: MP_PROGRAM //Program --> ProgramHeading ";" Block ".", rule #2 
+	{
+		ProgramHeading();
+		Match(MP_SCOLON);
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::ProgramHeading()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
 
 // example for Block. 
 // here "lookahead" is the lookahead token. We should enumerate the tokens so that they can be used in switch statements
@@ -18,9 +75,9 @@ parser::block()
 	{
 		case MP_VAR: // when MP_VAR Block -> "var" VariableDeclaration ";" VariableDeclarationTail, rule #4
 		{
-			Match("var");
+			Match(MP_VAR);
 			variableDeclaration();
-			Match(";"); //maybe char not string?
+			Match(MP_SCOLON);
 			VariableDeclarationTail();
 			break();
 		}
@@ -31,7 +88,332 @@ parser::block()
 		}
 }
 
-parser::match(string token)
+	// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::VariableDeclarationPart()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::VariableDeclarationTail()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::VariableDeclaration()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::Type()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::ProcedureAndFunctionDeclarationPart()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::ProcedureDeclaration()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::FunctionDeclaration()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::ProcedureHeading()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::FunctionHeading()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::OptionalFormalParameterList()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::FormalParameterSectionTail()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::FormalParameterSection()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::ValueParameterSection()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::VariableParameterSection()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::StatementPart()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::CompoundStatement()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::StatementSequence()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::StatementTail()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+// precondition: (not sure what this should be, but is necessary)
+// postcondition: (not sure what this should be, but is necessary)
+parser::Statement()
+{
+	switch(lookahead)
+	{
+	case: _
+	{
+	}
+	default: //everything else
+		{
+			Error();
+			break;
+		}
+	}
+}
+
+ 
+
+parser::match(enum token)//probably not correct syntax
 {
 	//puts a token on the tree
 	//gets the next lookahead
