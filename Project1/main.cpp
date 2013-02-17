84748c8b7463461a0da48692c563c2b5aaa3509d
smorrisonsmith <sarah.morrisonsmith@gmail.com>
2013-02-17 14:06:57 -0700

    Changed output message to meet milestone 5 req
    "After matching the end of file marker in the <system_goal> rule of the
    grammar, your parser should print the message "The input program
    parses!""

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -17,7 +17,7 @@ int main ( int argc, char* argv[] )
 
 	if (parser->Parse())
 	{
-		cout << "The program parses ok." << endl;
+		cout << "The input program parses!" << endl;
 	} 
 	
 	// press enter to exit
